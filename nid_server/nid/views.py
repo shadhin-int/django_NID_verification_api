09c7147490445ab7b34d3ab5aaa1e304129b08d9
shadhin-int <shadhin.int@gmail.com>
2021-02-14 01:09:48 +0600

    django nid verification project created

---
+++ nid_server/nid/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import NIDInfo
+
+
+def check_nid(request, nid_number):
+    is_exist = NIDInfo.objects.filter(nid_number=nid_number).exists()
+    return JsonResponse({'status': is_exist})
