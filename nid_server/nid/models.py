c69ef3cc0728731653e44f6ef116c9bd4224eb6f
shadhin-int <shadhin.int@gmail.com>
2021-02-18 01:46:28 +0600

    updated model

--- nid_server/nid/models.py
+++ nid_server/nid/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class NIDInfo(models.Model):
     nid_number = models.CharField(max_length=50)
     name = models.CharField(max_length=50)
+    contact_number = models.IntegerField()
 
     def __str__(self):
         return self.name
--- nid_server/nid/views.py
+++ nid_server/nid/views.py
@@ -8,3 +8,5 @@ from rest_framework.decorators import api_view
 def check_nid(request, nid_number):
     is_exist = NIDInfo.objects.filter(nid_number=nid_number).exists()
     return JsonResponse({'status': is_exist})
+
+
