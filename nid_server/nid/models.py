d20c7f66a9ba1da893f8be7f293932f3dd20e65a
shadhin-int <shadhin.int@gmail.com>
2021-02-18 02:08:27 +0600

    update model

--- nid_server/nid/models.py
+++ nid_server/nid/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class NIDInfo(models.Model):
     nid_number = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
     contact_number = models.IntegerField()
 
     def __str__(self):
